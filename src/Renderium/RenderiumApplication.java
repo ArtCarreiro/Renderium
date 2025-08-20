28d1404620390eaec5e835cd1713840d034d0bf0
Arthur Carreiro <167469828+ArtCarreiro@users.noreply.github.com>
2025-08-20 16:46:15 -0300

    Adicionando rotação na horizontal

--- src/Renderium/RenderiumApplication.java
+++ src/Renderium/RenderiumApplication.java
@@ -50,25 +50,48 @@ public class RenderiumApplication {
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
 
+                double heading = Math.toRadians(headingSlider.getValue());
+                double pitch = Math.toRadians(pitchSlider.getValue());
+
+                Matrix3 headingTransform = new Matrix3(new double[]{
+                        Math.cos(heading), 0, -Math.sin(heading),
+                        0, 1, 0,
+                        Math.sin(heading), 0, Math.cos(heading)
+                });
+
+                Matrix3 pitchTransform = new Matrix3(new double[]{
+                        1, 0, 0,
+                        0, Math.cos(pitch), Math.sin(pitch),
+                        0, -Math.sin(pitch), Math.cos(pitch)
+                });
+
+                Matrix3 transform = headingTransform.multiply(pitchTransform);
                 g2.translate(getWidth() / 2, getHeight() / 2);
 
                 for (Triangle t : tris) {
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
+
                     Path2D path = new Path2D.Double();
-                    path.moveTo(t.v1.x, t.v1.y);
-                    path.lineTo(t.v2.x, t.v2.y);
-                    path.lineTo(t.v3.x, t.v3.y);
+                    path.moveTo(v1.x, v1.y);
+                    path.lineTo(v2.x, v2.y);
+                    path.lineTo(v3.x, v3.y);
                     path.closePath();
 
                     g2.setColor(t.color);
-                    g2.fill(path);   // desenha preenchido
+                    g2.fill(path);
                     g2.setColor(Color.BLACK);
-                    g2.draw(path);   // borda preta
+                    g2.draw(path);
                 }
             }
         };
         pane.add(renderPanel, BorderLayout.CENTER);
 
-        frame.setSize(400, 400);
+        headingSlider.addChangeListener(e -> renderPanel.repaint());
+        pitchSlider.addChangeListener(e -> renderPanel.repaint());
+
+        frame.setSize(500, 500);
         frame.setVisible(true);
     }
 }
